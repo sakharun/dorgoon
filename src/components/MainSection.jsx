643a8a4f8a8fb1e8d1b7e6be97dc7a3088f0c9f0
IM <legentiy@yandex.ru>
2024-11-15 19:43:07 +0900

    срфта

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -4,7 +4,7 @@ export default function MainSection() {
   return (
     <section>
       <h3 className="centered">
-        ТаҥарАарАҕабар - Таҥарабар, Таҥараҕа үҥэн-сүгүрүйэн көрдөһөбүн дьиҥи
+        ТаҥарАарАҕабар - Таҥарабар, Таҥараҕа үҥэн-сүгүрүйэн көрдөһөбүн, дьиҥи
         билэрбэр-көрөрбөр.
       </h3>
       <h6 className="centered" style={{ color: "#666", opacity: "0.5" }}>
